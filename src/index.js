25f39546d338b9d80b28c347c20ad66585dfd597
Ned-el-ch <nedelchev.niki@gmail.com>
2020-04-08 15:50:58 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import App from './App';
 import './index.css';
 
 const store = createStore(
-  shoppingListItemReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	shoppingListItemReducer, 
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App store={store}/>
+	</Provider>,
+	document.getElementById('root')
 );
